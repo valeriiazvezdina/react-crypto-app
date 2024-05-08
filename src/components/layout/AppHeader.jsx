acf772fc6f4c25f49a10a180154580383b56df34
Valeriia Zvezdina <valeriia.zvezdina@gmail.com>
2024-05-08 23:46:29 +0200

    feat: modify header

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -1,16 +1,58 @@
-import { Layout } from 'antd';
+import { Layout, Select, Space, Button } from 'antd';
+import { useCrypto } from '../../context/crypto-context.jsx';
+import { useEffect, useState } from 'react';
 
 const headerStyle = {
+    width: '100%',
     textAlign: 'center',
-    color: '#fff',
     height: 60,
-    paddingInline: 48,
-    lineHeight: '64px',
-    backgroundColor: '#4096ff',
+    padding: '1rem',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center'
 };
-  
+
 export default function AppHeader() {
+    const [select, setSelect] = useState(false);
+    const { crypto } = useCrypto();
+
+    useEffect(() => {
+        const keypress = (event) => {
+            if (event.key === '/') setSelect(prev => !prev);
+        }
+        document.addEventListener('keypress', keypress);
+        return () => document.removeEventListener('keypress', keypress);
+    }, []);
+
+    function handleSelect(value) {
+        console.log(value)
+    }
+
     return (
-        <Layout.Header style={headerStyle}>Header</Layout.Header>
+        <Layout.Header style={headerStyle}>
+            <Select
+                style={{
+                    width: 250,
+                }}
+                value='press / to open'
+                optionLabelProp='label'
+                options={crypto.map(coin => ({
+                    label: coin.name,
+                    value: coin.id,
+                    icon: coin.icon
+                }))}
+                open={select}
+                onClick={() => setSelect(prev => !prev)}
+                onSelect={handleSelect}
+                optionRender={(option) => (
+                    <Space>
+                        <img src={option.data.icon} style={{width: 20}} alt={option.data.label}>
+                        </img>
+                        {option.data.label}
+                    </Space>
+                )}
+            />
+            <Button type='primary'>Add Asset</Button>
+        </Layout.Header>
     )
 }
--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -3,10 +3,10 @@ import AppHeader from './AppHeader.jsx';
 import AppContent from './AppContent.jsx';
 import AppSider from './AppSider.jsx';
 import { useContext } from 'react';
-import CryptoContext from '../../context/crypto-context.jsx';
+import { useCrypto } from '../../context/crypto-context.jsx';
 
 export default function AppLayout() {
-    const { loading } = useContext(CryptoContext);
+    const { loading } = useCrypto();
 
     if (loading) {
         return <Spin fullscreen />;

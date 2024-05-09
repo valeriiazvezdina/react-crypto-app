b6946f93dca85600e6ba9c221d356878ae5e67c9
Valeriia Zvezdina <valeriia.zvezdina@gmail.com>
2024-05-09 14:37:42 +0200

    feat: modify modal

--- src/components/CoinInfoModal.jsx
+++ src/components/CoinInfoModal.jsx
@@ -1,3 +1,49 @@
+import { Flex, Typography, Tag, Divider } from 'antd';
+
 export default function CoinInfoModal({ coin }) {
-    return <h2>{coin.name}</h2>;
+    return (
+        <>
+            <Flex align='center'>
+                <img src={coin.icon} alt={coin.name} style={{ width: 40, marginRight: 10 }} />
+                <Typography.Title level={2} style={{ margin: 0 }}>
+                    ({coin.symbol}) {coin.name}
+                </Typography.Title>
+            </Flex>
+
+            <Divider />
+
+            <Typography.Paragraph>
+                <Typography.Text strong>1 day: </Typography.Text>
+                <Tag color={coin.priceChange1h > 0 ? 'green' : 'red'}>{coin.priceChange1h}%</Tag>
+
+                <Typography.Text strong>1 week: </Typography.Text>
+                <Tag color={coin.priceChange1d > 0 ? 'green' : 'red'}>{coin.priceChange1d}%</Tag>
+
+                <Typography.Text strong>1 hour: </Typography.Text>
+                <Tag color={coin.priceChange1w > 0 ? 'green' : 'red'}>{coin.priceChange1w}%</Tag>
+            </Typography.Paragraph>
+
+            <Typography.Paragraph>
+                <Typography.Text strong>Price: </Typography.Text>
+                {coin.price.toFixed(2)}$
+            </Typography.Paragraph>
+
+            <Typography.Paragraph>
+                <Typography.Text strong>Price BTC: </Typography.Text>
+                {coin.priceBtc}
+            </Typography.Paragraph>
+
+            <Typography.Paragraph>
+                <Typography.Text strong>Market Cap: </Typography.Text>
+                {coin.marketCap}$
+            </Typography.Paragraph>
+
+            <Typography.Paragraph>
+                <Typography.Text strong>Contract Address: </Typography.Text>
+                {coin.contractAddress ? coin.contractAddress : 'N/A'}
+            </Typography.Paragraph>
+
+            {/* TODO: add icon links to social media of coins */}
+        </>
+    );
 }
